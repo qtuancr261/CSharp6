ccc8fba1ee005b60310fd75b754007ffa0144f66
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-07-05 09:58:34 +0700

    Add project files.

---
+++ Functions_CS6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_CS6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
