f1e06d81ad0cf0d8ed79ccfb46743e404df007e6
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-07-06 23:45:00 +0700

    Continue testing

--- Functions_CS6/Program.cs
+++ Functions_CS6/Program.cs
@@ -11,7 +11,17 @@ namespace Functions_CS6
         static void Main(string[] args)
         {
             Console.WriteLine("Testing github extension on Visual Studio 2017");
-            Console.WriteLine($"The sum of four values: 110, 520, 78, -120  is : {SumParaArrays(110, 520, 78, -120)}"); // a new feature from C# 6.0 
+            Console.WriteLine($"The sum of four values: 110, 520, 78, -120  is : {SumParaArrays(110, 520, 78, -120)}  {CS6.NewFeaturesCS6.StringInterpolation}"); // a new feature from C# 6.0 
+            Console.WriteLine("Or we can use this normal method: " + SumParaArrays(110, 520, 78, -120)); // normal method
+            double[] arrayA = new double[4];
+            foreach (double value in arrayA)
+            {
+                Console.Write($"{value} - > ");
+                //value = Convert.ToDouble(Console.ReadLine());
+                // foreach in C# is a read-only loop, you cannot change the values of any of the elements
+            }
+            InputArray(arrayA);
+            Console.WriteLine($"The sum of all values in arrayA: {SumArray(arrayA)}");
         }
         public static int SumParaArrays(params int[] vals)
         {
@@ -22,9 +32,24 @@ namespace Functions_CS6
             }
             return sum;
         }
+        public static void InputArray(double[] array)
+        {
+            Console.WriteLine("INPUT>>>>>>>>>");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Input element [{i}]: ");
+                array[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+        }
         public static double SumArray(double[] array)
         {
-            return 4.5;
+            double sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
         }
     }
 }
