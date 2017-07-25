00bac8ae6f987b9d57129c2c0e7a35134d413154
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-07-25 15:17:57 +0700

    >Test ref - out Parameters >Struct Value Types and Class Reference Types > Using .dll

--- Class_OOP/Program.cs
+++ Class_OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using System;
+using ClassLib;
 namespace Class_OOP
 {
     class Program
@@ -20,6 +21,25 @@ namespace Class_OOP
 
             Author Teddy = new Author("Teddy", 35);
             WriteLine(Teddy.GetName() + "\n-------");
+
+            MyExternalLib lib = new MyExternalLib();
+
+            int a = 10, b = 50;
+            WriteLine($"ref a + ref b = {lib.SumRef(ref a, ref b)} => a = {a} | b = {b}");
+
+            int c = 10, d = 50;
+            WriteLine($"out c + out d = {lib.SumOut(out c, out d)} => c = {c} | d = {d}");
+
+            MyStruct strt = new MyStruct();
+            strt.num = 10;
+            MyStruct strtCpy = strt;
+            strtCpy.num = 20;
+            WriteLine($"struct: str.num = {strt.num} | strCpy.num = {strtCpy.num}");
+
+            MyExternalLib libCpy = lib;
+            lib.info = "Thieu Quang Tuan";
+            libCpy.info = "Windows 10 Creator";
+            WriteLine($"class: lib.info = {lib.info} | libCpy.info = {libCpy.info}");
         }
     }
 }
