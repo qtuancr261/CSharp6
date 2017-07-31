3e94b9bb886e1b8139532e6ab6b05cd0d73bc0ed
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-07-31 12:18:19 +0700

    Test derived class with override methods

---
+++ Class_OOP/ITBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Class_OOP
+{
+    class ITBook : ABook
+    {
+        private WroxBookTopic topic;
+        public ITBook() : base()
+        {
+            WriteLine("ITBook default constructor");
+        }
+        public ITBook(string bookName, Author bookAuthor, double initPrice, double initDiscount, int initPages, WroxBookTopic bookTopic)
+            : base(bookName, bookAuthor, initPrice, initDiscount, initPages)
+        {
+            topic = bookTopic;
+            WriteLine("ITBook constructor with parameters");
+        }
+        public override double SalePrice()
+        {
+            return OriginalPrice - OriginalPrice * DiscountPercent;
+        }
+
+    }
+}
