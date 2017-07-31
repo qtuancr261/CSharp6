8b3b609f0135a3915ec8752f2f77a96f4cfc7074
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-07-31 12:17:17 +0700

    enum types for book categories

---
+++ Class_OOP/BookCategories.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_OOP
+{
+    enum AmazonBookCategories
+    {
+        Arts_Photography,
+        Biographies_Memoirs,
+        ChildrenBooks,
+        CookBooks,
+        History,
+        Literature_Fiction,
+        Mystery_Suspense,
+        Romance,
+        SciFi_Fanstaty,
+        Teen_YoungAdult
+    }
+    
+    enum WroxBookTopic
+    {
+        ASP_NET,
+        CSharp_CPlusPlus,
+        DataBase,
+        DotNET,
+        General,
+        Java,
+        Mac,
+        MicrosoftOffice,
+        MicrosoftServers,
+        Mobile,
+        OpenSource,
+        PHP_MySQL,
+    }
+}
