5c875e294dacf47450d6e983d755590ff91131de
thieuquangtuan <tuancr261@gmail.com>
2017-08-01 22:33:35 +0700

    Test abstract class - protected Constructor - Properties set => get => - #region #endregion

--- Class_OOP/ABook.cs
+++ Class_OOP/ABook.cs
@@ -8,16 +8,19 @@ namespace Class_OOP
 {
     abstract class ABook
     {
+        #region Fields
         private string name;
         private Author author;
         private double originalPrice;
         private double discountPercent; // %
         private readonly int pageNumbers;
-        public ABook()
+        #endregion
+        #region Constructor
+        protected ABook()
         {
             WriteLine("Abook default constructor");
         }
-        public ABook(string bookName = "Unknown", Author bookAuthor = null, double initPrice = 1000, double initDiscount = 0, int initPages = 50)
+        protected ABook(string bookName = "Unknown", Author bookAuthor = null, double initPrice = 1000, double initDiscount = 0, int initPages = 50)
         {
             name = bookName;
             author = bookAuthor;
@@ -26,11 +29,14 @@ namespace Class_OOP
             pageNumbers = initPages;
             WriteLine("Abook constructor with parameters");
         }
+        #endregion
+        #region Properties
         public string Name { get => name; set => name = value; }
         public double OriginalPrice { get => originalPrice; set => originalPrice = value; }
         public double DiscountPercent
-        { get => discountPercent;
-          set
+        {
+            get => discountPercent;
+            set
             {
                 try
                 {
@@ -39,7 +45,7 @@ namespace Class_OOP
                     else
                         throw new ArgumentException("Discount percent is invalid. Please provide another value for it: ");
                 }
-                catch(ArgumentException argExcep)
+                catch (ArgumentException argExcep)
                 {
                     WriteLine(argExcep.Message);
                     discountPercent = 0;
@@ -47,11 +53,10 @@ namespace Class_OOP
             }
         }
         public int PageNumbers { get => pageNumbers; }
-        internal Author Author { get => author; set => author = value; }
-
+        public Author Author { get => author; set => author = value; }
+        #endregion
+        #region AbstractMethods
         public abstract double SalePrice();
-
-
-
+        #endregion
     }
 }
